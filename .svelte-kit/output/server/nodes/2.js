225227e1a0d4fcaf5524fef90b66f6dd0f47d77f
Franz-Mueller <franz@franzhenrymueller.de>
2026-08-08 12:15:49 +0200

    chore: restore gitignore and remove generated files

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,6 +0,0 @@
-
-
-export const index = 2;
-export const imports = ["_app/immutable/nodes/2.BPWW9_lX.js","_app/immutable/chunks/BS96xuks.js","_app/immutable/chunks/xihTtKlq.js"];
-export const stylesheets = [];
-export const fonts = [];
--- build/_app/immutable/entry/start.DQsE41WQ.js
+++
@@ -1,1 +0,0 @@
-import{o as e,t}from"../chunks/BZWYavR7.js";export{e as load_css,t as start};
