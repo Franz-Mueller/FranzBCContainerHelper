729ce833888483d4775521655998e8d2b4c85394
Franz-Mueller <franz@franzhenrymueller.de>
2026-08-13 21:03:34 +0200

    fixed docker build image. finally ready for windows test even if code is just sad

--- src-tauri/src/docker/image.rs
+++ src-tauri/src/docker/image.rs
@@ -1,3 +1,4 @@
+use crate::utils::bc_version::BcVersion;
 use crate::utils::file_handling::copy_dir_all;
 use bollard::{body_full, query_parameters::BuildImageOptionsBuilder, Docker};
 use bytes::Bytes;
@@ -49,6 +50,31 @@ pub async fn build_image(artifact_path: &Path, artifact_url: &str) {
     let temp_navdvd_folder = Path::new(&temp_navdvd_folder);
     fs::create_dir(temp_navdvd_folder).unwrap();
 
+    let dbpath =
+        format!("{}/{}", artifact_path.to_str().unwrap(), manifest.database).replace("\\", "/");
+
+    let commondata = if BcVersion::from_str(&manifest.version) < BcVersion::from_str("27.0.33344.0")
+    {
+        "CommonAppData"
+    } else {
+        "CommApp"
+    };
+
+    let demo_db_dir = format!(
+        "./{}/SQLDemoDatabase/{}/Microsoft/Microsoft Dynamics NAV/ver/Database",
+        temp_navdvd_folder.to_str().unwrap(),
+        commondata
+    );
+
+    fs::create_dir_all(&demo_db_dir).unwrap();
+
+    let demo_db_path = format!("{}/database.bak", demo_db_dir);
+
+    dbg!(&dbpath);
+    dbg!(&demo_db_path);
+
+    fs::copy(&dbpath, &demo_db_path).unwrap();
+
     for entry in artifact_path.read_dir().unwrap() {
         match entry {
             Ok(entry) => {
@@ -59,7 +85,6 @@ pub async fn build_image(artifact_path: &Path, artifact_url: &str) {
                     "TestToolKit",
                     "UpgradeToolKit",
                     "Extensions",
-                    &manifest.database.to_string(),
                 ]
                 .contains(&file_name.to_str().unwrap())
                     || file_name.to_str().unwrap().starts_with("Applications")
@@ -67,7 +92,7 @@ pub async fn build_image(artifact_path: &Path, artifact_url: &str) {
                     dbg!(&file_name);
                     let destination = format!(
                         "{}/{}",
-                        temp_build_folder.to_str().unwrap(),
+                        temp_navdvd_folder.to_str().unwrap(),
                         file_name.into_string().unwrap()
                     );
                     println!("entry: {entry:?} | destination: {destination:?}");
@@ -96,7 +121,6 @@ pub async fn build_image(artifact_path: &Path, artifact_url: &str) {
     writeln!(dockerfile, "FROM {}", base_image).unwrap();
     writeln!(dockerfile, "ENV DatabaseServer=localhost DatabaseInstance=SQLEXPRESS DatabaseName=CRONUS IsBcSandbox={} artifactUrl={} filesOnly={}", is_bc_sandbox, artifact_url, false).unwrap();
     writeln!(dockerfile, "").unwrap();
-    writeln!(dockerfile, "COPY my /run/").unwrap();
     writeln!(dockerfile, "COPY NAVDVD /NAVDVD/").unwrap();
     writeln!(dockerfile, "").unwrap();
     writeln!(dockerfile, "RUN \\Run\\start.ps1 -installOnly").unwrap();
