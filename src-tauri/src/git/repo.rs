22d5b69ff8d08657bb34047e0fcb780a41521476
Franz-Mueller <franz@franzhenrymueller.de>
2026-08-19 15:26:02 +0200

    implemented clone from url

--- src-tauri/src/git/repo.rs
+++ src-tauri/src/git/repo.rs
@@ -1,19 +1,29 @@
-use std::path::{Path, PathBuf};
+use git2::Repository;
+use std::path::Path;
 use url::Url;
 
-struct Repo {
-    url: Url,
-    path: PathBuf,
+struct BcRepo {
+    repo: Repository,
 }
 
-impl Repo {
-    pub async fn clone(url: Url, repos_folder: PathBuf) -> Result<Self, RepoError> {
-        Ok(Self {
-            url: url,
-            path: repos_folder,
-        })
+impl BcRepo {
+    pub async fn clone_from_url(from_url: Url, into_folder: &Path) -> Result<Self, RepoError> {
+        let repo = match Repository::clone(&from_url.to_string(), &into_folder) {
+            Ok(repo) => repo,
+            Err(e) => {
+                return Err(RepoError::CloneFailed(
+                    from_url.to_string(),
+                    into_folder.display().to_string(),
+                    e,
+                ));
+            }
+        }; // TODO call as async operation // TODO map specific Error code for verbose output
+        Ok(Self { repo: repo })
     }
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum RepoError {}
+pub enum RepoError {
+    #[error("could not clone repo {0} into {1}: {2}")]
+    CloneFailed(String, String, git2::Error),
+}
